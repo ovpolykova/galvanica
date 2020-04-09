3d492119c15ce46fbd56350f9b00d8ba2932b70d
ovpolykova <47399078+ovpolykova@users.noreply.github.com>
2020-04-09 10:53:42 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,416 @@
+<?php 
+include 'template/head.php';
+include 'template/header_guest.php';
+include 'template/header.php';
+?>
+
+<div class="container">
+<div class="row">
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators animate-box intro-heading">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active" style="height:30px; border-radius: 50%;"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1" style="height:30px; border-radius: 50%;"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2" style="height:30px; border-radius: 50%;"></li>
+  </ol>
+  <div class="carousel-inner" role="listbox" id="content">
+    <div class="carousel-item active">
+    <img src="img/slider.png" width="100%" alt="">
+    <pre id="text1">Присоединитесь к СОЮЗУ <br> мастеров гальваники</pre>
+    <button type="button" id="but1">ПРИОБРЕСТИ КУРС </button>
+    </div>
+    <div class="carousel-item">
+      <img src="img/slider.png" width="100%" alt="">
+    <pre id="text1">Присоединитесь к СОЮЗУ <br> мастеров гальваники</pre>
+    <button type="button" id="but1">ПРИОБРЕСТИ КУРС </button>    
+    </div>
+    <div class="carousel-item">
+      <img src="img/slider.png" width="100%" alt="">
+    <pre id="text1">Присоединитесь к СОЮЗУ <br> мастеров гальваники</pre>
+    <button type="button" id="but1">ПРИОБРЕСТИ КУРС </button>
+    </div>
+  </div>
+</div>
+</div></div> 
+<div class="container">
+  <div class="row">
+    <div class="col-md" style="	background-color: #ffff;">
+    <pre id="text2">Что такое <br>СОЮЗ?</pre>
+  <p id="text3">
+  Союз мастеров гальваники — 
+  это объединение творческих людей с целью
+  общения, обмена опытом и обучения
+  в области прикладной гальванопластики.</p>
+  <a href="#" id="text4"> <u>ПОДРОБНЕЕ</u></a>
+    </div>
+    <div class="col-md">
+    <img src="img/Rectangle_167.png" width="100%" alt="">
+    <p id="text5"> Создание бизнеса с небольшими вложениями<br> и скорым результатом</p>
+    <p id="text6">Общение с единомышленниками <br> из разных городов</p>
+    <p id="text7">Реализация и воплощение творческих <br> идей и создание уникальных художественных <br> изделий</p>
+    </div>
+  </div>
+</div>
+<div class="container">
+  <div class="row" id="cont">
+  <div class="col-12">
+  <p id="text8">Как это работает?</p>
+  </div>  
+    <div class="col-md-auto">
+      <div style="width: 55.7px;
+height: 55.7px;
+left: 161.5px;
+top: 1413.78px;
+
+opacity: 0.1;
+/* bl */
+
+border: 2px solid #AFC0D7;
+box-sizing: border-box;
+border-radius: 5px;
+transform: matrix(0.71, -0.71, -0.71, -0.71, 0, 0);">
+      </div>
+      <div style="width: 68.59px;
+height: 68.59px;
+left: 161.5px;
+top: 1461.39px;
+
+/* bl */
+
+background: #AFC0D7;
+opacity: 0.1;
+/* bl */
+
+border: 1px solid #1F2122;
+box-sizing: border-box;
+border-radius: 5px;
+transform: matrix(0.71, -0.71, -0.71, -0.71, 0, 0);"></div>
+      <p id="text9">1</p>
+      <p id="text10">Стань</p>
+      <P id="text11">«НОВИЧКОМ»</P>
+      <P id="text12">Зарегистрируйтесь на платформе. <br>
+Получите бесплатное видео, <br> возможность приобретать <br> сопутствующие товары, а также <br> доступ к закрытому форуму среди <br> «новичков»</P>
+<button type="button" id="but2">РЕГИСТРАЦИЯ </button>    
+    </div>
+    <div class="col-md-auto">
+      <div style="width: 55.7px;
+height: 55.7px;
+left: 161.5px;
+top: 1413.78px;
+
+opacity: 0.1;
+/* bl */
+
+border: 2px solid #AFC0D7;
+box-sizing: border-box;
+border-radius: 5px;
+transform: matrix(0.71, -0.71, -0.71, -0.71, 0, 0);">
+      </div>
+      <div style="width: 68.59px;
+height: 68.59px;
+left: 161.5px;
+top: 1461.39px;
+
+/* bl */
+
+background: #AFC0D7;
+opacity: 0.1;
+/* bl */
+
+border: 1px solid #1F2122;
+box-sizing: border-box;
+border-radius: 5px;
+transform: matrix(0.71, -0.71, -0.71, -0.71, 0, 0);"></div>
+      <p id="text9">2</p>
+      <p id="text10">Перейди на</p>
+      <P id="text11">уровень "МАСТЕР"</P>
+      <P id="text12">Приобретите один курс. <br> Например, новичкам подойдёт <br> курс «Серебрение». <br>
+Получите сопровождение <br> и поддержку, доступ к закрытому форуму среди «МАСТЕРОВ».</P>
+<button type="button" id="but3">КУПИТЬ КУРС </button>    
+    </div>
+    <div class="col-md-auto">
+      
+    <div style="width: 55.7px;
+height: 55.7px;
+left: 161.5px;
+top: 1413.78px;
+
+opacity: 0.1;
+/* bl */
+
+border: 2px solid #AFC0D7;
+box-sizing: border-box;
+border-radius: 5px;
+transform: matrix(0.71, -0.71, -0.71, -0.71, 0, 0);">
+      </div>
+      <div style="width: 68.59px;
+height: 68.59px;
+left: 161.5px;
+top: 1461.39px;
+
+/* bl */
+
+background: #AFC0D7;
+opacity: 0.1;
+/* bl */
+
+border: 1px solid #1F2122;
+box-sizing: border-box;
+border-radius: 5px;
+transform: matrix(0.71, -0.71, -0.71, -0.71, 0, 0);"></div>
+      <p id="text9">3</p>
+      <p id="text10">Перейди на</p>
+      <P id="text11">уровень "VIP"</P>
+      <P id="text12">Приобретите весь пакет курсов. <br> Он стоит дешевле, чем покупать <br> курсы в отдельности. 
+       <br> Получите доступ к базе <br> сотрудников и ко всем форумам.</P>
+<button type="button" id="but3">КУПИТЬ КУРС </button>    
+    </div>
+  </div>
+</div>
+
+<div class="container">
+<div class="row">
+<div class="col-12">
+  <p id="kurs"> Популярные курсы</p>
+  </div> 
+  <li style="background-image: url(img/Rectangle_25.png);  margin-left:20px;  list-style-type: none;">		    
+    <div class="col-sm" id="i0" >
+    <div class="slider-text-inner text-center">
+				   					<div class="desc">
+                       <div id="Rectangle_165">
+                         <img src="img/iconmonstr-thumb-9_5.png" alt="" id="like">
+                         <label id="l15">15</label>
+                       </div>
+                       <div id="korz"> <a href="#"> <img src="img/iconmonstr-shopping-cart-26_4.png" id="kk" alt=""></a></div>
+				   						<span class="icon"><i class="flaticon-cutlery"></i></span>
+					   					<div id="Rectangle_83">
+                         <label id="sereb">«СЕРЕБРЕНИЕ» <br> 25 000 ₽</label>
+                       </div>
+					   					<div class="desc2"></div>
+				   					</div>
+				   				</div>
+    </div>
+</li>
+<li style="background-image: url(img/Rectangle_53.png);  margin-left:20px;  list-style-type: none;">		    
+    <div class="col-sm" id="i0" >
+    <div class="slider-text-inner text-center">
+				   					<div class="desc">
+                       <div id="Rectangle_165">
+                         <img src="img/iconmonstr-thumb-9_5.png" alt="" id="like">
+                         <label id="l15">15</label>
+                       </div>
+                       <div id="korz"> <a href="#"> <img src="img/iconmonstr-shopping-cart-26_4.png" id="kk" alt=""></a></div>
+				   						<span class="icon"><i class="flaticon-cutlery"></i></span>
+					   					<div id="Rectangle_83">
+                         <label id="sereb">«ЗОЛОЧЕНИЕ» <br> 25 000 ₽</label>
+                       </div>
+					   					<div class="desc2"></div>
+				   					</div>
+				   				</div>
+    </div>
+</li>
+<li style="background-image: url(img/Rectangle_54.png); margin-left:20px; list-style-type: none;">		    
+    <div class="col-sm" id="i0" >
+    <div class="slider-text-inner text-center">
+				   					<div class="desc">
+                       <div id="Rectangle_165">
+                         <img src="img/iconmonstr-thumb-9_5.png" alt="" id="like">
+                         <label id="l15">15</label>
+                       </div>
+                       <div id="korz"> <a href="#"> <img src="img/iconmonstr-shopping-cart-26_4.png" id="kk" alt=""></a></div>
+				   						<span class="icon"><i class="flaticon-cutlery"></i></span>
+					   					<div id="Rectangle_83">
+                         <label id="sereb">«МАСШТАБНОЕ НАРАЩИВАНИЕ МЕДИ» <br> 25 000 ₽</label>
+                       </div>
+					   					<div class="desc2"></div>
+                     </div>
+				   				</div>
+    </div> 
+</li>
+</div>
+</div>
+<div class="container">
+<div class="row">
+  <a href="#" id="kurs1">Все курсы</a></div>
+</div>
+
+<div class="container">
+<div class="row" style="background: rgba(31, 33, 34, 0.8);">
+<div class="col-12">
+  <p id="kurs" style="color: #ffff"> Популярные товары</p>
+  </div>  
+  
+  <li style="background-image: url(img/church257-5310-0-big_1.png); background-repeat: no-repeat;  list-style-type: none;">		    
+    <div class="col" id="Rectangle_107" >
+    <div class="slider-text-inner">
+				   					<div class="desc" >
+                       <div id="korz2"> <a href="#"> <img src="img/iconmonstr-shopping-cart-26_4.png" id="kk2" style="margin-left:10px;" alt=""></a></div>
+				   						<span class="icon"><i class="flaticon-cutlery"></i></span>
+					   					<div id="Rectangle_83_1">
+                         <label id="pobed">Георгий победоносец</label> <label>4 000 ₽</label>
+                       </div>
+					   					<div class="desc2"></div>
+				   					</div>
+				   				</div>
+    </div>
+    </li>  &ensp;&ensp;&ensp;
+<li style="background-image: url(img/church257-5310-0-big_2.png); background-repeat: no-repeat; list-style-type: none;">		    
+    <div class="col" id="Rectangle_107" >
+    <div class="slider-text-inner ">
+				   					<div class="desc">
+                     <div id="korz2"> <a href="#"> <img src="img/iconmonstr-shopping-cart-26_4.png" id="kk2" alt="" style="margin-left:10px;"></a></div>
+				   						<span class="icon"><i class="flaticon-cutlery"></i></span>
+                       <div id="Rectangle_83_1">
+                         <label id="pobed"> &ensp; Иконка 1 &ensp;&ensp;&ensp;&ensp;&ensp; 3 500 ₽</label>
+                       </div>
+					   					<div class="desc2"></div>
+				   					</div>
+				   				</div>
+    </div>
+</li> &ensp;&ensp;&ensp;
+
+<div class="card" style="margin-top: 13px;">
+    <img class="card-img-top" src="img/church257-5310-0-big_3.png" style="" alt="Card image cap">
+      <div id="korz2" style="margin-left: 500px; margin-top:-50px;"> <a href="#"> 
+        <img src="img/iconmonstr-shopping-cart-26_4.png" id="kk2" style="margin-left:10px;" alt=""></a></div>
+    <br><br><br><br><br><br>
+                        <div class="card-footer" style="background: #575C63;  " >
+                         <label id="pobed" style="color: #ffff;"> &ensp; Тайная <br> вечеря &ensp;&ensp;&ensp;&ensp;&ensp; 20 000 ₽</label>
+                       </div>
+					   					
+  </div>
+
+  <li style="background-image: url(img/church257-5310-0-big_4.png); margin-top: -350px; background-repeat: no-repeat; list-style-type: none;">		    
+<div class="col" id="Rectangle_107" >
+    <div class="slider-text-inner ">
+				   					<div class="desc">
+                     <div id="korz2"> <a href="#"> <img src="img/iconmonstr-shopping-cart-26_4.png" id="kk2" alt="" style="margin-left:10px;"></a></div>
+				   						<span class="icon"><i class="flaticon-cutlery"></i></span> <br>
+                       <div id="Rectangle_83_1">
+                         <label id="pobed"> &ensp; Иконка 1 &ensp;&ensp;&ensp;&ensp;&ensp; 3 500 ₽</label>
+                       </div>
+					   					<div class="desc2"></div>
+				   					</div>
+				   				</div>
+    </div>
+</li> &ensp;&ensp;&ensp;
+<li style="background-image: url(img/church257-5310-0-big5.png); margin-top: -350px; background-repeat: no-repeat; list-style-type: none;">		    
+<div class="col" id="Rectangle_107" >
+    <div class="slider-text-inner ">
+				   					<div class="desc">
+                     <div id="korz2"> <a href="#"> <img src="img/iconmonstr-shopping-cart-26_4.png" id="kk2" alt="" style="margin-left:10px;"></a></div>
+				   						<span class="icon"><i class="flaticon-cutlery"></i></span>
+                       <div id="Rectangle_83_1" style="margin-top: 205px;">
+                         <label id="pobed"> &ensp; Иконка 1 &ensp;&ensp;&ensp;&ensp;&ensp; 3 500 ₽</label>
+                       </div>
+					   					<div class="desc2"></div>
+				   					</div>
+				   				</div>
+    </div>
+    </div> 
+</li>
+</div>
+</div>
+</div>
+<br>
+<div class="container">
+<div class="row">
+  <a href="#" id="kurs1">Все курсы</a></div>
+</div>
+
+<!--топ мастеров-->
+<div class="container">
+<div class="row">
+<div class="col-12">
+  <p id="kurs"> Топ мастеров</p>
+  </div> 
+  <li style="background-image: url(img/Rectangle_52.png); width: 255px; margin-left:10px; background-repeat: no-repeat;   list-style-type: none;">		    
+    <div class="col-sm">
+    <div class="slider-text-inner text-center">
+				   					<div class="desc">
+                     <div id="Group_129"> <label id="Group_129_1">МАСТЕР</label> </div>   
+                     <div id="Group_129_2"> <label id="Group_129_3">Ретинг: 88 %</label> </div>   
+                     <span class="icon"><i class="flaticon-cutlery"></i></span>
+                     <div>
+                       <img src="img/trophy-shape4.png" id="img10" alt="">
+                       <label id="name">Анна <br> &nbsp; &nbsp; Петрова</label>
+                     </div>
+					   					<div class="desc2"></div>
+				   					</div>
+				   				</div>
+    </div>
+</li> 
+<li style="background-image: url(img/Rectangle_106.png); width: 255px; margin-left:10px; background-repeat: no-repeat; list-style-type: none;">		    
+    <div class="col-sm">
+    <div class="slider-text-inner text-center">
+				   					<div class="desc">
+                       
+                		 <div id="Group_129"> <label id="Group_129_1">МАСТЕР</label> </div>   
+                     <div id="Group_129_2"> <label id="Group_129_3">Ретинг: 68 %</label> </div>   
+                     <span class="icon"><i class="flaticon-cutlery"></i></span>
+                     <div>
+                       <img src="img/trophy-shape4.png" id="img10" alt="">
+                       <label id="name">Антон <br> &nbsp; &nbsp; Павлов</label>
+                     </div>
+					   				
+					   					<div class="desc2"></div>
+				   					</div>
+				   				</div>
+    </div>
+</li> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<li style="background-image: url(img/Rectangle_107.png); width: 255px; margin-left:10px; background-repeat: no-repeat; list-style-type: none;">		    
+    <div class="col-sm">
+    <div class="slider-text-inner text-center">
+				   					<div class="desc">
+                       
+              			 <div id="Group_129"> <label id="Group_129_1">МАСТЕР</label> </div>   
+                     <div id="Group_129_2"> <label id="Group_129_3">Ретинг: 66 %</label> </div>   
+                     <span class="icon"><i class="flaticon-cutlery"></i></span>
+                     <div>
+                       <img src="img/trophy-shape4.png" id="img10" alt="">
+                       <label id="name">Иван <br> &nbsp; &nbsp; Смерягин</label>
+                     </div>
+					   					<div class="desc2"></div>
+                     </div>
+				   				</div>
+    </div> 
+</li> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<li style="background-image: url(img/Rectangle_108.png); width: 255px;  background-repeat: no-repeat;  list-style-type: none;">		    
+    <div class="col-sm">
+    <div class="slider-text-inner text-center">
+				   					<div class="desc">
+                    
+                     <div id="Group_129"> <label id="Group_129_1">НОВИЧОК</label> </div>   
+                     <div id="Group_129_2"> <label id="Group_129_3">Ретинг: 54 %</label> </div>   
+                     <span class="icon"><i class="flaticon-cutlery"></i></span>
+                     <div>
+                       <img src="img/trophy-shape4.png" id="img10" alt="">
+                       <label id="name">Маргарита <br> &nbsp; &nbsp; Алексеевна</label>
+                     </div>
+					   					<div class="desc2"></div>
+                     </div>
+				   				</div>
+    </div> 
+</li>
+</div>
+</div>
+
+<!--на платформе-->
+<div class="container">
+<div class="row">
+  <li style="background-image: url(img/Rectangle_166.png); background-repeat: no-repeat;  width:100%;  list-style-type: none;">		    
+
+    <div class="slider-text-inner text-center">
+				   					<div class="desc">
+                    <img src="img/Rectangle_168.png" alt="" style="padding-left: 250px;">
+                     <span class="icon"><i class="flaticon-cutlery"></i></span>
+                   
+                     
+					   					<div class="desc2"></div>
+				   					</div>
+				   				</div>
+    
+</li> 
+</div>
+</div>
+
+<?php include 'template/footer.php' ?>
